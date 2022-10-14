f8e170ab6cf575259f75046cb2fd65deebdf5791
Wadabee <bh.jpvw@gmail.com>
2022-10-14 20:35:58 +0900

    replaced nivo to echarts

--- src/pages/tide-graph/index.tsx
+++ src/pages/tide-graph/index.tsx
@@ -2,63 +2,55 @@ import dayjs from "dayjs";
 import { NextPage } from "next";
 import tideData from "../../feature/tide/data/json/2022_hososhima.json";
 
-import { Line } from "@nivo/line";
 import { JmaTide } from "../../@types/tide";
+
+import ReactECharts from "echarts-for-react";
+import _ from "lodash";
 
 const TideGraph: NextPage = () => {
   const today = dayjs().format("YYYY-MM-DD");
   const tideDataForToday = (tideData as JmaTide)[today];
 
-  const commonProperties = {
-    width: 900,
-    height: 400,
-    margin: { top: 20, right: 20, bottom: 60, left: 80 },
-    // 26    data,
-    animate: true,
-    // enableSlices: "x",
-  };
-
   return (
     <>
       Tide Graph
       {JSON.stringify(tideDataForToday)}
-      <Line
-        {...commonProperties}
-        curve="monotoneX"
-        data={[
-          {
-            id: "tide",
-            data: tideDataForToday.tidePerHour.map((value, index) => ({
-              x: index,
-              y: value,
-            })),
+      <ReactECharts
+        option={{
+          xAxis: {
+            name: "Time",
+            type: "category",
+            data: _.range(25).map((value) => `${value}:00`),
+            boundaryGap: false,
           },
-        ]}
-        xScale={{
-          type: "linear",
-          min: 0,
-          max: 23,
-          nice: true,
+          yAxis: {
+            name: "Tide (cm)",
+            type: "value",
+            min: 0,
+            max: 240,
+          },
+          series: [
+            {
+              data: tideDataForToday.tidePerHour,
+              type: "line",
+              smooth: true,
+              lineStyle: {
+                width: 5,
+              },
+              // markPoint: {
+              //   symbol: "roundRect",
+              //   data: [
+              //     {
+              //       value: "aaa",
+              //       name: "highTide",
+              //       coord: [1, 100],
+              //     },
+              //   ],
+              // },
+            },
+          ],
         }}
-        yScale={{
-          type: "linear",
-          min: 0,
-          max: 240,
-        }}
-        axisLeft={{
-          legend: "Tide(cm)",
-          legendOffset: 12,
-        }}
-        axisBottom={{
-          legend: "Time",
-          legendOffset: -12,
-        }}
-        lineWidth={4}
-        enableArea={true}
-        enablePoints={false}
-        gridXValues={[0, 3, 6, 9, 12, 15, 18, 21, 24]}
-        colors={{ scheme: "blues" }}
-      />
+      ></ReactECharts>
     </>
   );
 };
