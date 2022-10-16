e3a6311beb9176ae34fc03259c5c7ea10668dade
Wadabee <bh.jpvw@gmail.com>
2022-10-16 19:58:04 +0900

    added MUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import MenuIcon from "@mui/icons-material/Menu";
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar variant="dense">
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Fishing App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ mt: 7 }}>
+        <Component {...pageProps} />
+      </Box>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
