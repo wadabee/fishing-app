faa03bf45c93ac36c0f17eb3eaaf263d91659e9e
Wadabee <bh.jpvw@gmail.com>
2022-10-17 14:22:16 +0900

    added link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  return <>Home</>;
+  return (
+    <>
+      <div>Home</div>
+      <Link href="/tide-graph">
+        <a>Tide Graph</a>
+      </Link>
+    </>
+  );
 };
 
 export default Home;
